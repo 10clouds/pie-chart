1be8618c514d3f8aa79d51055059ac0ca9c1167b
annapersona <anna.persona@10clouds.com>
2016-04-06 13:55:51 +0200

    Setting default value for duration

--- dist/pie-chart.js
+++ dist/pie-chart.js
@@ -208,7 +208,7 @@ getTools: function(dimensions, options) {
 addLegend: function(svg) {
   var items = svg.append("g")
     .attr("id", "n3-pie-legend");
-  
+
   return this;
 },
 
@@ -218,7 +218,7 @@ updateLegend: function(svg, data, dimensions, options) {
   } else {
     this.updateRegularLegend(svg, data, dimensions, options);
   }
-  
+
   return this;
 },
 
@@ -226,25 +226,25 @@ updateRegularLegend: function(svg, data, dimensions, options) {
   var that = this;
   var radius = this.getRadius(dimensions);
   var availableWidth = radius - options.thickness;
-  
+
   var legendHalfHeight = data.length*availableWidth/10;
-  
+
   var scale = d3.scale.linear()
     .range([-legendHalfHeight, legendHalfHeight])
     .domain([0, data.length-1])
     .nice()
-  
-  
+
+
   var items = svg.selectAll("#n3-pie-legend")
     .selectAll(".legend-item")
       .data(data, function(d) {return d.label;});
-  
+
   items.enter()
     .append("text")
       .classed("legend-item", true)
       .on("mouseover", this.onMouseOver(svg))
       .on("mouseout", this.onMouseOut(svg))
-  
+
   items
     .text(this.getLegendLabelFunction(availableWidth))
     .attr({
@@ -265,16 +265,16 @@ updateRegularLegend: function(svg, data, dimensions, options) {
 
 updateGaugeLegend: function(svg, data, dimensions, options) {
   var size = (this.getRadius(dimensions) - options.thickness)/2;
-  
+
   if (size < 0) {
     return;
   }
-  
+
   var legend = svg.selectAll("#n3-pie-legend");
-  
+
   var title = legend.selectAll(".legend-title")
     .data(data.filter(function(s) {return !s.__isComplement;}));
-  
+
   title.enter()
     .append("text")
     .attr({
@@ -288,20 +288,20 @@ updateGaugeLegend: function(svg, data, dimensions, options) {
       "fill-opacity": 0.8
     })
   ;
-  
+
   title.text(function(d) {return d.label;});
-  
+
   title.exit().remove();
-  
+
   var value = legend.selectAll(".legend-value")
     .data(data.filter(function(s) {return !s.__isComplement;}));
-  
+
   value.enter()
     .append("text")
     .attr({
       "class": "legend-value",
       "text-anchor": "middle",
-      "y": size/1.5 + "px" 
+      "y": size/1.5 + "px"
     })
     .style({
       "font-size": size + "px",
@@ -309,23 +309,23 @@ updateGaugeLegend: function(svg, data, dimensions, options) {
       "fill-opacity": 0.8
     })
   ;
-  
+
   value
     .text(function(d) {return d.value + (d.suffix || '');});
-  
+
   value.exit().remove();
 },
 
 getLegendLabelFunction: function(availableWidth) {
   var that = this;
-  
+
   return function(datum, index) {
     if (availableWidth < 40) {
       return "";
     } else if (availableWidth < 100) {
       return datum.label;
     }
-    
+
     return that.getLegendLabel(datum.label, datum.value, 20);
   }
 },
@@ -337,7 +337,7 @@ onMouseOver: function(svg) {
         "opacity": function(d, i) {return i === index ? "1" : "0.4";}
       });
     }
-    
+
     dimOrHighlight(svg.selectAll(".legend-item"));
     dimOrHighlight(svg.selectAll(".arc"));
   };
@@ -350,7 +350,7 @@ onMouseOut: function(svg) {
         "opacity": 1
       });
     }
-    
+
     show(svg.selectAll(".legend-item"));
     show(svg.selectAll(".arc"));
   };
@@ -361,15 +361,16 @@ getLegendLabel: function(label, value, totalLength) {
   for (var i = 0; i < totalLength; i++) {
     dots.push(".");
   }
-  
+
   label = label + " ";
   value = " " + value;
-  
+
   label.split("").forEach(function(c, i) {dots[i] = c;});
   value.split("").forEach(function(c, i) {dots[totalLength - value.length + i] = c;});
-  
+
   return dots.join("");
-},
+}
+,
 
 increase_brightness: function (hex, percent) {
   hex = hex.replace(/^\s*#|\s*$/g, '');
@@ -466,12 +467,12 @@ sanitizeOptions: function(options) {
   } else {
     options.thickness = parseInt(options.thickness);
   }
-  
-  
+
+
   if (options.mode === "gauge") {
     this.sanitizeGaugeOptions(options);
   }
-  
+
   return options;
 },
 
@@ -481,7 +482,14 @@ sanitizeGaugeOptions: function(options) {
   } else {
     options.total = parseInt(options.total);
   }
+
+  if (options.duration === undefined) {
+    options.duration = 1200;
+  } else {
+    options.duration = parseInt(options.duration);
+  }
 }
+
 
   };
 }])
